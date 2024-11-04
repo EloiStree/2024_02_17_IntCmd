983afb3f825282af420a8b48f874437388219afc
Eloi Strée <eloistree@gmail.com>
2024-11-04 07:17:46 +0100

    Save: 11/04/2024 7:17 AM

---
+++ Runtime/IntCmdMono_FilterByDoubleDigitsType.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Eloi.IID
+{
+    /// <summary>
+    /// I am class that filter integer of type DD12345678 where DD filter the passing number.
+    /// </summary>
+    public class IntCmdMono_FilterByDoubleDigitsType : MonoBehaviour
+    {
+
+        public FilterDoubleDigits[] m_filters;
+
+        [System.Serializable]
+        public class FilterDoubleDigits
+        {
+            public int m_mustStartWith = 12;
+            public UnityEvent<int> m_onIntEvent;
+
+        }
+
+        [Header("Debug")]
+        public int m_lastStartWithValue = 0;
+        public int m_lastValue = 0;
+
+
+        public void PushIn(I_IntCmdGet integer) { 
+            if(integer!=null)
+                PushIn(integer.GetValue());
+        }
+
+        public void PushIn(int value)
+        {
+            int startDigit = value / 100000000;
+            m_lastStartWithValue = startDigit;
+            m_lastValue = value;
+            foreach (FilterDoubleDigits f in m_filters)
+            {
+                if (f.m_mustStartWith == startDigit)
+                {
+                    f.m_onIntEvent.Invoke(value);
+                }
+            }
+        }
+    
+    }
+}
+
--- Runtime/IntCmdRelayMono.cs
+++ Runtime/IntCmdRelayMono.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Eloi.IID
 {
+
+
     [System.Serializable]
     public class IntCmdRelayMono : AbstractIntCmdHolderMono
     {
         public IntCmd m_lastValue = new IntCmd();
         public IntCmdEvent m_onIntCmdEvent;
+        public UnityEvent<int> m_onIntEvent;
         public void PushIn(string text)
         {
             if (int.TryParse(text, out int value))
@@ -14,28 +18,33 @@ namespace Eloi.IID
                 m_lastValue.SetValue(value);
 
                 m_onIntCmdEvent.Invoke(m_lastValue);
+                m_onIntEvent.Invoke(value);
             }
         }
         public void PushIn(int value)
         {
             m_lastValue.SetValue(value);
             m_onIntCmdEvent.Invoke(m_lastValue);
+            m_onIntEvent.Invoke(value);
         }
         public void PushIn(IntCmd value)
         {
             m_lastValue.SetValue(value.GetValue());
             m_onIntCmdEvent.Invoke(value);
+            m_onIntEvent.Invoke(value.GetValue());
         }
 
         [ContextMenu("Repush Value In Inspector")]
         public void RepushValueInInspector()
         {
             m_onIntCmdEvent.Invoke(m_lastValue);
+            m_onIntEvent.Invoke(m_lastValue.GetValue());
         }
 
         public override void NotifyChildrenValueChanged()
         {
             m_onIntCmdEvent.Invoke(m_lastValue);
+            m_onIntEvent.Invoke(m_lastValue.GetValue());
         }
 
         public override I_IntCmd GetChildrenIntCmd()
